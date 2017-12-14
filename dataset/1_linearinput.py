9433018548df1a3deeb69f3a82c69a94170b2a65
dominhhai <minhhai3b@gmail.com>
2017-12-14 13:09:03 +0900

    dataset: add linear input one variable

--- dataset/1_linearinput.py
+++ dataset/1_linearinput.py
@@ -5,22 +5,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 DIST_FILE_NAME = os.path.splitext(__file__)[0] + '.csv'
-N_SIGMA = 0.15
+N_SIGMA = 550
 
 # input vector x
-X = np.random.rand(100)
+X = np.random.random_integers(1000, 2000, 100)
 X.sort()
-# output vector Y=2pi * X
-Y = np.sin(2 * np.pi * X)
+# output vector Y=4X+3
+Y = 4*X + 3
 # make noise by normal distribution: mu = y
-Y += N_SIGMA * np.random.randn(Y.shape[0])
+Y = N_SIGMA * np.random.randn(Y.shape[0]) + Y
 
 # build DataFrame
 df = pd.DataFrame(data=Y, index=X, columns=['y'])
 # write down to CSV file
 df.to_csv(DIST_FILE_NAME, header=False)
-print('... sin(2*pi) data ...')
+print('... 4x+3 data ...')
 
 # plot
-df.plot(legend=False, marker='o', color='g', mec='b', mfc='w')
+df.plot(legend=False, marker='o', color='w', mec='b', mfc='w')
 plt.xlabel('x'); plt.ylabel('y'); plt.show()
