25628770f9f040da4a58aed47141ab94b94135d0
dominhhai <minhhai3b@gmail.com>
2017-12-15 15:58:33 +0900

    one_var_sin2pi

--- code/linear_regression/one_var_linearinput.py
+++ code/linear_regression/one_var_linearinput.py
@@ -17,21 +17,20 @@ plt.xlabel('x'); plt.ylabel('y'); plt.show()
 X = df.values[:,0].reshape((-1,1))
 Y = df.values[:,2]
 # add X_0(1,..,1) to X
-X = np.concatenate((np.ones((X.shape[0],1)), X), axis=1)
-# print X.shape, Y.shape
+_X = np.concatenate((np.ones((X.shape[0],1)), X), axis=1)
+# print _X.shape, Y.shape
 
 # parameters learning
-# W_hat = np.dot(np.linalg.pinv(X), Y)
-W_hat = np.dot(np.linalg.inv(np.dot(X.T,X)), np.dot(X.T, Y))
-print 'W_hat :=', W_hat
+# W = np.dot(np.linalg.pinv(_X), Y)
+W = np.dot(np.linalg.pinv(np.dot(_X.T,_X)), np.dot(_X.T, Y))
+print 'W :=', W
 
 # plot result
 # training data
 df.plot(x='x', y='y', legend=False, marker='o', style='o', mec='b', mfc='w')
 # expected line
-plt.plot(df.values[:,0], df.values[:,1], color='g')
+plt.plot(X, df.values[:,1], color='g')
 # predicted line
-X_hat = np.linspace(X[:,1].min(), X[:,1].max(), 10)
-Y_hat = W_hat[0] + W_hat[1] * X_hat
-plt.plot(X_hat, Y_hat, color='r')
+Y_hat = np.dot(_X, W)
+plt.plot(X, Y_hat, color='r')
 plt.xlabel('x'); plt.ylabel('y'); plt.show()
--- code/linear_regression/one_var_sin2pi.py
+++ code/linear_regression/one_var_sin2pi.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 DATA_FILE_NAME = './dataset/1_sin2pi.csv'
+DEGREE = 3 # degree of polynomial of X
 
 # load data
 df = pd.read_csv(DATA_FILE_NAME)
@@ -14,24 +15,25 @@ plt.plot(df.values[:,0], df.values[:,1], color='g')
 plt.xlabel('x'); plt.ylabel('y'); plt.show()
 
 # extract X, Y
-X = df.values[:,0].reshape((-1,1))
+X = df.values[:,0]
 Y = df.values[:,2]
-# add X_0(1,..,1) to X
-X = np.concatenate((np.ones((X.shape[0],1)), X), axis=1)
-# print X.shape, Y.shape
+# build polynomial X
+_X = np.empty((X.shape[0], DEGREE+1))
+for i in range(DEGREE + 1):
+    _X[:,i] = np.power(X,i)
+# print _X.shape, Y.shape
 
 # parameters learning
-# W_hat = np.dot(np.linalg.pinv(X), Y)
-W_hat = np.dot(np.linalg.inv(np.dot(X.T,X)), np.dot(X.T, Y))
-print 'W_hat :=', W_hat
+# W = np.dot(np.linalg.pinv(X), Y)
+W = np.dot(np.linalg.pinv(np.dot(_X.T,_X)), np.dot(_X.T, Y))
+print 'W :=', W
 
 # plot result
 # training data
 df.plot(x='x', y='y', legend=False, marker='o', style='o', mec='b', mfc='w')
 # expected line
-plt.plot(df.values[:,0], df.values[:,1], color='g')
+plt.plot(X, df.values[:,1], color='g')
 # predicted line
-X_hat = np.linspace(X[:,1].min(), X[:,1].max(), 10)
-Y_hat = W_hat[0] + W_hat[1] * X_hat
-plt.plot(X_hat, Y_hat, color='r')
+Y_hat = np.dot(_X,W)
+plt.plot(X, Y_hat, color='r')
 plt.xlabel('x'); plt.ylabel('y'); plt.show()
